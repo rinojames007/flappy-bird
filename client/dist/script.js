ef26746739fd557c6e39d84aa2665793a522b4d2
rinojames007 <rinojames.krl@gmail.com>
2023-05-30 11:33:44 +0530

    pipe init and minor bug fix

--- client/dist/script.js
+++ client/dist/script.js
@@ -4,7 +4,7 @@ const birdElement = document.querySelector('#bird');
 // extract the css property of bird
 const birdStyle = window.getComputedStyle(birdElement);
 // in order to make the bird down smoothly
-const gravity = 1;
+const gravity = 1.9;
 const velocity = 70;
 // since we have to make the bird jump and so on, therefore we have to extract the top value
 let positionY = parseInt(birdStyle.getPropertyValue('top').slice(0, 5));
@@ -19,7 +19,7 @@ function jump() {
         setTimeout(() => {
             birdElement.style.transform = 'rotate(0deg)';
         }, 10);
-        birdElement.style.transform = 'rotate(-20deg)';
+        birdElement.style.transform = 'rotate(-30deg)';
     }
 }
 // an event listener that will make the bird jump once space bar is pressed
@@ -40,10 +40,10 @@ function fall() {
 }
 function animate() {
     fall();
-    if (birdElement.style.top === '900px') {
+    if (birdElement.style.top === '850px') {
         cancelAnimationFrame(animationId);
         animationId = 0;
-        birdElement.style.top = '900px';
+        birdElement.style.top = '850px';
         return;
     }
     ;
@@ -51,3 +51,36 @@ function animate() {
     // executing this works, but the animation does not begin even after pressing space, although bird jumps
 }
 animate();
+// pipe
+const pipeContainer = document.getElementById('pipe-container');
+const pipeElement = document.createElement('img');
+let positionX = 10;
+let pipeSpeed = 1;
+let pipes = [];
+let pipeNumber = 0;
+function createPipe() {
+    let pipeElement = document.createElement('img');
+    // pipe init
+    pipeElement.setAttribute('class', 'pipe');
+    pipeElement.src = "/client/assets/pipe.png";
+    //setting height and width
+    pipeElement.setAttribute('width', '200px');
+    pipeElement.setAttribute('height', '40%');
+    //pipe insertion
+    pipes.push(pipeElement);
+    pipeContainer.appendChild(pipes[pipeNumber]);
+    pipeNumber++;
+    console.log(pipeNumber + "on the createPipe()");
+}
+setInterval(() => {
+    createPipe();
+}, 3000);
+console.log(pipeElement);
+function movePipe() {
+    positionX += pipeSpeed;
+    console.log(pipeNumber + " on the movePipe()");
+    pipeElement.style.right = positionX + 'px';
+    requestAnimationFrame(movePipe);
+}
+;
+movePipe();
--- client/src/script.ts
+++ client/src/script.ts
@@ -5,7 +5,7 @@ const birdElement: any = document.querySelector<HTMLElement>('#bird');
 const birdStyle:CSSStyleDeclaration = window.getComputedStyle(birdElement);
 
 // in order to make the bird down smoothly
-const gravity: number = 1;
+const gravity: number = 1.9;
 const velocity: number = 70;
 
 // since we have to make the bird jump and so on, therefore we have to extract the top value
@@ -24,7 +24,7 @@ function jump(): void {
         setTimeout(()=>{
             birdElement.style.transform = 'rotate(0deg)';
         }, 10);
-        birdElement.style.transform = 'rotate(-20deg)';
+        birdElement.style.transform = 'rotate(-30deg)';
     }
 }
 
@@ -48,10 +48,10 @@ function fall(): void {
 
 function animate(): void {
     fall();
-    if (birdElement.style.top === '900px') {
+    if (birdElement.style.top === '850px') {
         cancelAnimationFrame(animationId);
         animationId = 0;
-        birdElement.style.top = '900px';
+        birdElement.style.top = '850px';
         return;
     };
     animationId = requestAnimationFrame(animate);
@@ -59,3 +59,43 @@ function animate(): void {
 }
 
 animate();
+
+// pipe
+const pipeContainer: any = document.getElementById('pipe-container');
+const pipeElement: HTMLElement | any = document.createElement('img');
+
+let positionX = 10;
+let pipeSpeed: number = 1;
+let pipes: HTMLElement[] = [];
+let pipeNumber:number = 0;
+
+function createPipe(): void {
+    let pipeElement: HTMLElement | any = document.createElement('img');
+
+    // pipe init
+    pipeElement.classList.add('pipe');
+    pipeElement.src = "/client/assets/pipe.png";
+
+    //setting height and width
+    pipeElement.setAttribute('width', '200px');
+    pipeElement.setAttribute('height', '40%');
+
+    //pipe insertion
+    pipes.push(pipeElement);
+    pipeContainer.appendChild(pipes[pipeNumber]);
+    pipeNumber++;
+    console.log(pipeNumber + "on the createPipe()")
+
+}
+setInterval(()=> {
+    createPipe();
+}, 3000);
+// console.log(pipeElement);
+// function movePipe(): void {
+//     positionX += pipeSpeed;
+//     console.log(pipeNumber + " on the movePipe()")
+//     pipeElement.style.right = positionX + 'px';
+//     requestAnimationFrame(movePipe);
+// };
+
+// movePipe();
