38b84dd01a507fb59f6f1e228d0ac86b34280efd
rinojames007 <rinojames.krl@gmail.com>
2023-05-30 18:22:23 +0530

    #

--- client/dist/script.js
+++ client/dist/script.js
@@ -10,10 +10,17 @@ const velocity = 70;
 let positionY = parseInt(birdStyle.getPropertyValue('top').slice(0, 5));
 let isJumped = false;
 let animationId;
+// pipe
+const pipeContainer = document.getElementById('pipe-container');
+// an array which will stores the generated pipes
+let upperPipes = [];
+let lowerPipes = [];
+let pipeNumber = 0;
+const screenHeight = window.innerHeight;
 // this is this jumping stuff
 function jump() {
-    isJumped = true;
-    if (isJumped) {
+    if (!isJumped) {
+        isJumped = true;
         positionY -= velocity;
         birdElement.style.top = positionY + 'px';
         setTimeout(() => {
@@ -22,6 +29,27 @@ function jump() {
         birdElement.style.transform = 'rotate(-30deg)';
     }
 }
+// function for the bird to come down
+function fall() {
+    if (isJumped) {
+        positionY += gravity;
+        birdElement.style.top = positionY + 'px';
+    }
+}
+function animate() {
+    fall();
+    if (birdElement.style.top === '850px') {
+        cancelAnimationFrame(animationId);
+        animationId = 0;
+        birdElement.style.top = '850px';
+        isJumped = false;
+        return;
+    }
+    ;
+    animationId = requestAnimationFrame(animate);
+    // executing this works, but the animation does not begin even after pressing space, although bird jumps
+}
+animate();
 // an event listener that will make the bird jump once space bar is pressed
 document.addEventListener('keydown', (event) => {
     if (event.code === 'Space') {
@@ -33,42 +61,28 @@ document.addEventListener('keydown', (event) => {
         jump();
     }
 });
-// function for the bird to come down
-function fall() {
-    positionY += gravity;
-    birdElement.style.top = positionY + 'px';
-}
-function animate() {
-    fall();
-    if (birdElement.style.top === '850px') {
-        cancelAnimationFrame(animationId);
-        animationId = 0;
-        birdElement.style.top = '850px';
-        return;
-    }
-    ;
-    animationId = requestAnimationFrame(animate);
-    // executing this works, but the animation does not begin even after pressing space, although bird jumps
-}
-animate();
-// pipe
-const pipeContainer = document.getElementById('pipe-container');
-// an array which will stores the generated pipes
-let pipes = [];
-let pipeNumber = 0;
+//upper generation
 function createPipe() {
-    let pipeElement = document.createElement('img');
+    let upperElement = document.createElement('img');
+    let lowerElement = document.createElement('img');
     // pipe init
-    pipeElement.classList.add('pipe');
-    pipeElement.src = "assets/pipe.png";
+    upperElement.classList.add('pipe-upper');
+    lowerElement.classList.add('pipe-lower');
+    upperElement.src = "assets/pipe.png";
+    lowerElement.src = "assets/pipe.png";
     //setting height and width
-    pipeElement.setAttribute('width', '200px');
-    pipeElement.setAttribute('height', '40%');
+    upperElement.setAttribute('width', '400px');
+    upperElement.setAttribute('height', '40%');
+    lowerElement.setAttribute('width', '400px');
+    lowerElement.setAttribute('height', '40%');
     //pipe insertion
-    pipes.push(pipeElement);
-    pipeContainer.appendChild(pipes[pipeNumber]);
+    upperPipes.push(upperElement);
+    lowerPipes.push(lowerElement);
+    pipeContainer.appendChild(upperPipes[pipeNumber]);
+    pipeContainer.appendChild(lowerPipes[pipeNumber]);
     pipeNumber++;
 }
+;
 setInterval(() => {
     createPipe();
 }, 3000);
--- client/src/script.ts
+++ client/src/script.ts
@@ -1,24 +1,26 @@
 // get the bird element
 const birdElement: any = document.querySelector<HTMLElement>('#bird');
-
 // extract the css property of bird
 const birdStyle:CSSStyleDeclaration = window.getComputedStyle(birdElement);
-
 // in order to make the bird down smoothly
 const gravity: number = 1.9;
 const velocity: number = 70;
-
 // since we have to make the bird jump and so on, therefore we have to extract the top value
 let positionY: number = parseInt(birdStyle.getPropertyValue('top').slice(0, 5));
-
 let isJumped: boolean = false;
-
 let animationId: number;
+// pipe
+const pipeContainer: any = document.getElementById('pipe-container');
+// an array which will stores the generated pipes
+let upperPipes: HTMLElement[] = [];
+let lowerPipes: HTMLElement[] = [];
+let pipeNumber:number = 0;
+const screenHeight: number = window.innerHeight;
 
 // this is this jumping stuff
 function jump(): void {
-    isJumped = true;
-    if(isJumped) {
+    if(!isJumped) {
+        isJumped = true;
         positionY -= velocity;
         birdElement.style.top = positionY + 'px';
         setTimeout(()=>{
@@ -27,6 +29,28 @@ function jump(): void {
         birdElement.style.transform = 'rotate(-30deg)';
     }
 }
+
+// function for the bird to come down
+function fall(): void {
+    if(isJumped){
+    positionY += gravity;
+    birdElement.style.top = positionY + 'px';
+    }
+}
+
+function animate(): void {
+    fall();
+    if (birdElement.style.top === '850px') {
+        cancelAnimationFrame(animationId);
+        animationId = 0;
+        birdElement.style.top = '850px';
+        isJumped = false;
+    };
+    animationId = requestAnimationFrame(animate);
+    // executing this works, but the animation does not begin even after pressing space, although bird jumps
+}
+
+animate();
 
 // an event listener that will make the bird jump once space bar is pressed
 document.addEventListener('keydown', (event: KeyboardEvent) => {
@@ -40,49 +64,33 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
     }
 })
 
-// function for the bird to come down
-function fall(): void {
-    positionY += gravity;
-    birdElement.style.top = positionY + 'px';
-}
 
-function animate(): void {
-    fall();
-    if (birdElement.style.top === '850px') {
-        cancelAnimationFrame(animationId);
-        animationId = 0;
-        birdElement.style.top = '850px';
-        return;
-    };
-    animationId = requestAnimationFrame(animate);
-    // executing this works, but the animation does not begin even after pressing space, although bird jumps
-}
-
-animate();
-
-// pipe
-const pipeContainer: any = document.getElementById('pipe-container');
-// an array which will stores the generated pipes
-let pipes: HTMLElement[] = [];
-let pipeNumber:number = 0;
-
+//upper generation
 function createPipe(): void {
-    let pipeElement: HTMLElement | any = document.createElement('img');
+    let upperElement: HTMLElement | any = document.createElement('img');
+    let lowerElement: HTMLElement | any = document.createElement('img');
 
     // pipe init
-    pipeElement.classList.add('pipe');
-    pipeElement.src = "assets/pipe.png";
+    upperElement.classList.add('pipe-upper');
+    lowerElement.classList.add('pipe-lower');
+    upperElement.src = "assets/pipe.png";
+    lowerElement.src = "assets/pipe.png";
 
     //setting height and width
-    pipeElement.setAttribute('width', '200px');
-    pipeElement.setAttribute('height', '40%');
+    upperElement.setAttribute('width', '400px');
+    upperElement.setAttribute('height', '40%');
+
+    lowerElement.setAttribute('width', '400px');
+    lowerElement.setAttribute('height', '40%');
 
     //pipe insertion
-    pipes.push(pipeElement);
-    pipeContainer.appendChild(pipes[pipeNumber]);
+    upperPipes.push(upperElement);
+    lowerPipes.push(lowerElement);
+    pipeContainer.appendChild(upperPipes[pipeNumber]);
+    pipeContainer.appendChild(lowerPipes[pipeNumber]);
     pipeNumber++;
-}
+};
+
 setInterval(()=> {
     createPipe();
 }, 3000);
-
