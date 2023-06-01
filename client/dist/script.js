47c56f5f944821601dae0ce1cceaf24a3b37ec65
rinojames007 <rinojames.krl@gmail.com>
2023-06-01 12:37:17 +0530

    pipe animation patch

--- client/dist/script.js
+++ client/dist/script.js
@@ -8,19 +8,12 @@ const gravity = 1.9;
 const velocity = 70;
 // since we have to make the bird jump and so on, therefore we have to extract the top value
 let positionY = parseInt(birdStyle.getPropertyValue('top').slice(0, 5));
-let isJumped = false;
+let isJumping = false;
 let animationId;
-// pipe
-const pipeContainer = document.getElementById('pipe-container');
-// an array which will stores the generated pipes
-let upperPipes = [];
-let lowerPipes = [];
-let pipeNumber = 0;
-const screenHeight = window.innerHeight;
 // this is this jumping stuff
 function jump() {
-    if (!isJumped) {
-        isJumped = true;
+    if (!isJumping) {
+        isJumping = true;
         positionY -= velocity;
         birdElement.style.top = positionY + 'px';
         setTimeout(() => {
@@ -28,24 +21,28 @@ function jump() {
         }, 10);
         birdElement.style.transform = 'rotate(-30deg)';
     }
+    else {
+        fall();
+    }
 }
 // function for the bird to come down
 function fall() {
-    if (isJumped) {
-        positionY += gravity;
-        birdElement.style.top = positionY + 'px';
-    }
+    positionY += gravity;
+    birdElement.style.top = positionY + 'px';
 }
 function animate() {
-    fall();
-    if (birdElement.style.top === '850px') {
-        cancelAnimationFrame(animationId);
-        animationId = 0;
-        birdElement.style.top = '850px';
-        isJumped = false;
-        return;
+    if (isJumping) {
+        fall();
+        if (birdElement.style.top === '850px') {
+            cancelAnimationFrame(animationId);
+            animationId = 0;
+            birdElement.style.top = '850px';
+            return;
+        }
     }
-    ;
+    else {
+        isJumping = false;
+    }
     animationId = requestAnimationFrame(animate);
     // executing this works, but the animation does not begin even after pressing space, although bird jumps
 }
@@ -61,7 +58,20 @@ document.addEventListener('keydown', (event) => {
         jump();
     }
 });
-//upper generation
+function generatePipeHeight() {
+    // Define the minimum and maximum heights for the pipes
+    const minHeight = 30;
+    const maxHeight = 45;
+    // Generate a random height for the pipe
+    const pipeHeight = `${Math.floor(Math.random() * (maxHeight - minHeight + 1)) + minHeight}%`;
+    return pipeHeight;
+}
+// pipe
+const pipeContainer = document.getElementById('pipe-container');
+// an array which will stores the generated pipes
+let pipes = [];
+let pipeNumber = 0;
+//pipe generation
 function createPipe() {
     let upperElement = document.createElement('img');
     let lowerElement = document.createElement('img');
@@ -72,17 +82,16 @@ function createPipe() {
     lowerElement.src = "assets/pipe.png";
     //setting height and width
     upperElement.setAttribute('width', '400px');
-    upperElement.setAttribute('height', '40%');
+    upperElement.style.height = generatePipeHeight();
     lowerElement.setAttribute('width', '400px');
-    lowerElement.setAttribute('height', '40%');
+    lowerElement.style.height = generatePipeHeight();
     //pipe insertion
-    upperPipes.push(upperElement);
-    lowerPipes.push(lowerElement);
-    pipeContainer.appendChild(upperPipes[pipeNumber]);
-    pipeContainer.appendChild(lowerPipes[pipeNumber]);
-    pipeNumber++;
+    pipes.push(upperElement);
+    pipes.push(lowerElement);
+    pipeContainer.appendChild(pipes[0]);
+    pipeContainer.appendChild(pipes[1]);
+    pipes = [];
 }
-;
 setInterval(() => {
     createPipe();
 }, 3000);
--- client/src/script.ts
+++ client/src/script.ts
@@ -1,51 +1,53 @@
 // get the bird element
 const birdElement: any = document.querySelector<HTMLElement>('#bird');
+
 // extract the css property of bird
 const birdStyle:CSSStyleDeclaration = window.getComputedStyle(birdElement);
+
 // in order to make the bird down smoothly
 const gravity: number = 1.9;
 const velocity: number = 70;
+
 // since we have to make the bird jump and so on, therefore we have to extract the top value
 let positionY: number = parseInt(birdStyle.getPropertyValue('top').slice(0, 5));
-let isJumped: boolean = false;
+
+let isJumping: boolean = false;
+
 let animationId: number;
-// pipe
-const pipeContainer: any = document.getElementById('pipe-container');
-// an array which will stores the generated pipes
-let upperPipes: HTMLElement[] = [];
-let lowerPipes: HTMLElement[] = [];
-let pipeNumber:number = 0;
-const screenHeight: number = window.innerHeight;
 
 // this is this jumping stuff
 function jump(): void {
-    if(!isJumped) {
-        isJumped = true;
+    if(!isJumping) {
+        isJumping = true;
         positionY -= velocity;
         birdElement.style.top = positionY + 'px';
         setTimeout(()=>{
             birdElement.style.transform = 'rotate(0deg)';
         }, 10);
         birdElement.style.transform = 'rotate(-30deg)';
+    } else {
+        fall();
     }
 }
 
 // function for the bird to come down
 function fall(): void {
-    if(isJumped){
     positionY += gravity;
     birdElement.style.top = positionY + 'px';
-    }
 }
 
 function animate(): void {
-    fall();
-    if (birdElement.style.top === '850px') {
-        cancelAnimationFrame(animationId);
-        animationId = 0;
-        birdElement.style.top = '850px';
-        isJumped = false;
-    };
+    if(isJumping){
+        fall();
+        if (birdElement.style.top === '850px') {
+            cancelAnimationFrame(animationId);
+            animationId = 0;
+            birdElement.style.top = '850px';
+            return;
+        }
+    } else {
+        isJumping = false;
+    }
     animationId = requestAnimationFrame(animate);
     // executing this works, but the animation does not begin even after pressing space, although bird jumps
 }
@@ -64,8 +66,25 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
     }
 })
 
+function generatePipeHeight() {
+    // Define the minimum and maximum heights for the pipes
+    const minHeight = 30;
+    const maxHeight = 45;
 
-//upper generation
+    // Generate a random height for the pipe
+    const pipeHeight = `${Math.floor(Math.random() * (maxHeight - minHeight + 1)) + minHeight}%`;
+
+    return pipeHeight;
+}
+
+
+// pipe
+const pipeContainer: any = document.getElementById('pipe-container');
+// an array which will stores the generated pipes
+let pipes: HTMLElement[] = [];
+let pipeNumber: number = 0;
+
+//pipe generation
 function createPipe(): void {
     let upperElement: HTMLElement | any = document.createElement('img');
     let lowerElement: HTMLElement | any = document.createElement('img');
@@ -78,18 +97,19 @@ function createPipe(): void {
 
     //setting height and width
     upperElement.setAttribute('width', '400px');
-    upperElement.setAttribute('height', '40%');
+    upperElement.style.height = generatePipeHeight();
 
     lowerElement.setAttribute('width', '400px');
-    lowerElement.setAttribute('height', '40%');
+    lowerElement.style.height = generatePipeHeight();
 
     //pipe insertion
-    upperPipes.push(upperElement);
-    lowerPipes.push(lowerElement);
-    pipeContainer.appendChild(upperPipes[pipeNumber]);
-    pipeContainer.appendChild(lowerPipes[pipeNumber]);
-    pipeNumber++;
-};
+    pipes.push(upperElement);
+    pipes.push(lowerElement);
+    pipeContainer.appendChild(pipes[0]);
+    pipeContainer.appendChild(pipes[1]);
+    pipes = [];
+
+}
 
 setInterval(()=> {
     createPipe();
